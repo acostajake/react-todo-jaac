21a9cbf7a438e2fd7b26ccd89d74e6be5573f847
Jake Acosta <jakeacosta@Jakes-MacBook-Pro.local>
2019-11-25 19:25:49 -0800

    celebrate

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -4,7 +4,7 @@ export const TodoItems = ({ items, onDelete }) => {
   return (
     <ul className="task-list" data-testid="task-list">
       {items.map(item => (
-        <li onClick={() => onDelete(item.key)} key={item.key}>
+        <li onClick={() => onDelete(item)} key={items.indexOf(item)}>
           {item.text}
         </li>
       ))}
